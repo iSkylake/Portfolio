79bcb6a046593f422702b790059f729179014c3a
Llorch <skylakebeta@gmail.com>
2016-05-10 14:00:09 -0700

    Install JSON package, express, body-parse, mongoose, ejs. Add views folder and index.ejs

---
+++ server.js
@@ -0,0 +1,16 @@
+var mongoose = require("mongoose"),
+	bodyParser = require("body-parser"),
+	express = require("express"),
+	app = express();
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res){
+	res.render("index");
+});
+
+app.listen(3000, function(){
+	console.log("Server Started")
+});
