2d4878101a0daf27fcf41da3a53e4a760dff9844
Llorch <skyakebeta@gmail.com>
2016-05-26 19:10:57 -0700

    Add comment for db connect

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var mongoose = require("mongoose"),
 //Routes
 var indexRoutes = require("./routes/index");
 
+//Connect to database (deply || local)
 mongoose.connect(process.env.DATABASEURL || "mongodb://localhost/portfolio");
 
 app.set("view engine", "ejs"); //Looks for ejs files
